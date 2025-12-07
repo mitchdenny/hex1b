c1534519cdd6a14a29b1051947a56cac45700967
Mitch Denny <mitch@mitchdeny.com>
2025-12-07 12:56:45 +1100

    Websocket sample bootstrap.

--- apphost.cs
+++ apphost.cs
@@ -1,5 +1,9 @@
 #:sdk Aspire.AppHost.Sdk@13.1.0-preview.1.25605.5
 
+#pragma warning disable ASPIRECSHARPAPPS001
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddCSharpApp("websockets", "./samples/websockets");
 
 builder.Build().Run();
