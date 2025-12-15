53ef7597fe94eec9d4e33f79c14ae200063754a6
Mitch Denny <mitch@mitchdeny.com>
2025-12-15 14:43:23 +1100

    Get apphost configured.

--- apphost.cs
+++ apphost.cs
@@ -1,13 +1,20 @@
-﻿#:package Aspire.Hosting.DevTunnels@13.2.0-preview.1.25611.1
-#:sdk Aspire.AppHost.Sdk@13.2.0-preview.1.25611.1
+﻿#:sdk Aspire.AppHost.Sdk@13.2.0-preview.1.25611.1
+#:package Aspire.Hosting.JavaScript@13.2.0-preview.1.25611.1
+#:package Aspire.Hosting.Docker@13.2.0-preview.1.25611.1
 
 #pragma warning disable ASPIRECSHARPAPPS001
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var website = builder.AddCSharpApp("website", "./src/Hex1b.Website");
+builder.AddDockerComposeEnvironment("env");
 
-var tunnel = builder.AddDevTunnel("tunnel")
-    .WithReference(website);
+var website = builder.AddCSharpApp("website", "./src/Hex1b.Website")
+    .WithExternalHttpEndpoints();
+
+var content = builder.AddViteApp("content", "./src/content")
+    .WithReference(website)
+    .WaitFor(website);
+
+website.PublishWithContainerFiles(content, "./wwwroot");
 
 builder.Build().Run();
